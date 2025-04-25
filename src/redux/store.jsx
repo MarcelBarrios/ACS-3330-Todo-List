03988ff5488a339630e94bdc97ad945a2342bc59
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-04-24 21:11:33 -0400

    step 7 done

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,20 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 import filtersReducer from "./filtersSlice";
+import sortingReducer from "./sortingSlice"; // 1️⃣
 import { loadState, saveState } from "./localStorage";
-import { FILTERS } from "./filtersSlice";
 
-const preloadedState = loadState() || { todos: [], filters: FILTERS.ALL };
-console.log("Preloaded State in store:", preloadedState);
+const preloadedState = loadState() || { todos: [] };
 
 export const store = configureStore({
     reducer: {
         todos: todoReducer,
         filters: filtersReducer,
+        sorting: sortingReducer, // 2️⃣
     },
     preloadedState,
 });
 
 store.subscribe(() => {
-    saveState(store.getState());
+    saveState({
+        todos: store.getState().todos,
+        filters: store.getState().filters,
+        sorting: store.getState().sorting // 3️⃣
+    });
 });
